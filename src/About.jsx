7a7f0f666bd0d903f38700f48da95e02ace148c6
Renanmrqs <renanmarques1923@gmail.com>
2026-07-01 10:22:04 -0300

    tentando fazer o botao

--- src/About.jsx
+++ src/About.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react"
 
 function About( {about} ) {
-    const [hidenAbout, SetHiddenAbout] = useState(false)
+    const [hidenAbout, setHiddenAbout] = useState(false)
+    
+    function aboutSliced(about) {
+        if (hidenAbout == false) {
+            return about.slice(0, 2)
+        } else {
+            return about
+        }
+    }
+        
+    const arrayMap = aboutSliced(about) 
     
     return (
         <div className="about">
         <h1>Minha Jornada</h1>
                  
-            {about.map(hidenAbout === 'false' ? (about.slice(1,2)) :(about) => (
+            {about.map( (arrayMap) => (
             <div className="card-about" key={about.title}> 
                 <h3 className="title-about">{about.title} - {about.year}</h3>
                 <h2 className="subtitle-about">{about.subtitle}</h2>
                 <p className="description-about">{about.description}</p>
             </div>
             ))}                
-        <button onClick={(e) => SetHiddenAbout(true)}>Ver mais sobre</button>
+        <button onClick={() => setHiddenAbout(false)}>
+            Ver mais 
+            </button>
         </div>
     )
 }
